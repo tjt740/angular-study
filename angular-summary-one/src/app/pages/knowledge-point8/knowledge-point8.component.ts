6a87a4d218695a85a5518d0f0b04ffeaba1cc86a
tjt740 <15751666937@163.com>
2022-02-02 19:48:40 +0800

    formGroup复习

--- angular-summary-one/src/app/pages/knowledge-point8/knowledge-point8.component.ts
+++ angular-summary-one/src/app/pages/knowledge-point8/knowledge-point8.component.ts
@@ -1,12 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, Validators, FormGroup, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-knowledge-point8',
   templateUrl: './knowledge-point8.component.html',
   styleUrls: ['./knowledge-point8.component.less'],
 })
 export class KnowledgePoint8Component implements OnInit {
-  constructor() { }
+  demoForm = new FormGroup({});
+
+  constructor(
+    private fb: FormBuilder
+  ) {
+    this.demoForm = this.fb.group({
+      name: this.fb.control('谭金涛', [Validators.required]),
+      sex: new FormControl('男'),
+      age: [22,Validators.required]
+    })
+  }
   name = null;
   ngOnInit(): void { }
 
